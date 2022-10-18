c0b2dac7d6e157a078fc425b696b626349c7c672
郝勇 <howyong0419@hotmail.com>
2022-10-18 16:54:18 +0800

    ceshi

--- src/test/java/com/hy/springlearning/SpringLearningApplicationTests.java
+++ src/test/java/com/hy/springlearning/SpringLearningApplicationTests.java
@@ -1,13 +1,19 @@
 package com.hy.springlearning;
 
+import com.hy.springlearning.controller.IOController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringLearningApplicationTests {
 
+    @Autowired
+    IOController ioController;
+
     @Test
     void contextLoads() {
+        System.out.println(ioController);
     }
 
 }
