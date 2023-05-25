c40cee2f629dfb51d5c5c1dc363ed644f46ff579
郝勇 <howyong0419@hotmail.com>
2023-05-25 12:00:43 +0800

    添加编译期注解示例，未完成编译期生成代码的功能

--- demo/src/main/java/com/hy/demo/entity/Student.java
+++ demo/src/main/java/com/hy/demo/entity/Student.java
@@ -8,7 +8,14 @@ public class Student {
     private Integer age;
 
     public static void main(String[] args) {
-
+        for (String arg : args) {
+            System.out.println("参数----------》"+arg);
+        }
+        //打包后执行以下命令
+//        D:\projects\hy\SpringLearning\demo\target>java -jar demo.jar 123 456 789
+//        参数----------》123
+//        参数----------》456
+//        参数----------》789
     }
 
 }
---
+++ demo/src/main/java/com/hy/demo/entity/Teacher.java
@@ -0,0 +1,12 @@
+package com.hy.demo.entity;
+
+import com.hy.basejava.lombok.Data;
+
+@Data
+public class Teacher {
+    private String tname;
+    private Integer tage;
+
+
+
+}
