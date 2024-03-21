a6397303499ea30a98c694f49c455dc6da1f0973
haoyong <howyong0419@hotmail.com>
2024-03-21 08:29:42 +0800

    动态规划:背包问题

--- base-java/src/main/java/com/hy/basejava/dp/ChooseBag.java
+++ base-java/src/main/java/com/hy/basejava/dp/ChooseBag.java
@@ -8,10 +8,13 @@ public class ChooseBag {
 
     public static void main(String[] args) {
 
-        test1();
+        test2();
     }
 
 
+    /**
+     * 01背包
+     */
     public static void test1(){
         int maxWeight = 20;
         int[] weightArr = new int[]{1,2,20,5,8,4,3};
@@ -57,6 +60,53 @@ public class ChooseBag {
             }
             System.out.println();
         }
+    }
+
+    /**
+     * 01背包空间优化版
+     */
+    public static void test2(){
+        int maxWeight = 20;
+        int[] weightArr = new int[]{1,2,20,5,8,4,3};
+        int[] valueArr = new int[]{10,3,25,5,8,5,4};
+
+        int[] result = new int[maxWeight+1];
+
+        // 将前i件物品装入限重为j的背包内
+        for (int i = 0; i < weightArr.length; i++) {
+            for (int j = maxWeight; j > 0; j--) {
+                if(i==0){
+                    // 如果放入：剩余的承重
+                    int lastWeight = maxWeight - weightArr[i];
+                    // 能放入
+                    if(lastWeight<=maxWeight){
+                        // 当前背包承重大于当前物品重量表示可以放入 价值为当前物品的价值
+                        if (j >= weightArr[i]){
+                            result[j] = valueArr[i];
+                        }else{
+                            //假设背包承重大于剩余承重 代表未放入
+                            result[j] = 0;
+                        }
+                    }
+
+                }else{
+                    // 如果放上 那么放上后当前背包承重如下
+                    int lastWeight = j - weightArr[i];
+                    // 能放开
+                    if(lastWeight>=0){
+                        //当前的最优解            不捡            捡了（要从假设前一件物品剩余了足够的承重这一条件的最高价值的基础上＋当前价值）
+                        result[j] = Math.max(result[j],result[lastWeight]+valueArr[i]);
+                    }else{
+                        result[j] = result[j];
+                    }
+                }
+
+            }
+        }
+            for (int anInt : result) {
+                System.out.print(anInt+"\t");
+            }
+
     }
 
 
